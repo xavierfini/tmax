2da0b7a909d34e900f09df8351056b82768eea06
xavierfini <108425006+xavierfini@users.noreply.github.com>
2024-04-20 13:42:32 +0200

    test

---
+++ scriptconnexion.js
@@ -0,0 +1,18 @@
+// script.js
+
+var users = {
+    "admin": { "password": "admin", "redirect": "admin.html" },
+    // Ajoutez d'autres utilisateurs au besoin
+};
+
+function validateForm() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if (users.hasOwnProperty(username) && users[username].password === password) {
+        // Rediriger l'utilisateur vers la page spécifiée dans l'objet users
+        window.location.href = users[username].redirect;
+    } else {
+        alert('Nom d\'utilisateur ou mot de passe incorrect.');
+    }
+}
