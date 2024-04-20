84f59eaa42205e0502f06fddaef2c0b1287a2969
xavierfini <108425006+xavierfini@users.noreply.github.com>
2024-04-20 12:10:32 +0200

    frfrtfrr

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
-const expand_btn = document.querySelector(".expand-btn");
 
-let activeIndex;
+let loginForm = document.querySelector(".my-form");
 
-expand_btn.addEventListener("click", () => {
-  document.body.classList.toggle("collapsed");
-});
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
 
-const current = window.location.href;
-
-const allLinks = document.querySelectorAll(".sidebar-links a");
-
-allLinks.forEach((elem) => {
-  elem.addEventListener("click", function () {
-    const hrefLinkClick = elem.href;
-
-    allLinks.forEach((link) => {
-      if (link.href == hrefLinkClick) {
-        link.classList.add("active");
-      } else {
-        link.classList.remove("active");
-      }
-    });
-  });
-});
-
-const searchInput = document.querySelector(".search__wrapper input");
-
-searchInput.addEventListener("focus", (e) => {
-  document.body.classList.remove("collapsed");
+    console.log('Email:', email.value);
+    console.log('Password:', password.value);
+    // process and send to API 
 });
