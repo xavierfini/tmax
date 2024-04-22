c1ce68628fccbd84dd15256409ddb77a7b3a3935
xavierfini <108425006+xavierfini@users.noreply.github.com>
2024-04-22 18:20:30 +0200

    MAJ

--- scriptconnexion.js
+++ scriptconnexion.js
@@ -1,18 +1,26 @@
-// script.js
-
-var users = {
-    "xavier.fini": { "password": "Xavier", "redirect": "login=xavier-fini.html" },
+// Définition des utilisateurs avec leurs identifiants, mots de passe et pages dédiées
+var users = [
+    { username: "xavierfini", password: "Xavier14", redirectUrl: "xavierfini.html" },
+    { username: "utilisateur2", password: "motdepasse2", redirectUrl: "page2.html" },
     // Ajoutez d'autres utilisateurs au besoin
-};
+];
 
-function validateForm() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Empêcher le formulaire de se soumettre
 
-    if (users.hasOwnProperty(username) && users[username].password === password) {
-        // Rediriger l'utilisateur vers la page spécifiée dans l'objet users
-        window.location.href = users[username].redirect;
+    // Récupérer les valeurs de l'identifiant et du mot de passe
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Vérifier l'authentification pour chaque utilisateur
+    var authenticatedUser = users.find(function(user) {
+        return user.username === username && user.password === password;
+    });
+
+    if (authenticatedUser) {
+        // Rediriger vers la page dédiée de l'utilisateur authentifié
+        window.location.href = authenticatedUser.redirectUrl;
     } else {
-        alert('Nom d\'utilisateur ou mot de passe incorrect.');
+        alert("Identifiant ou mot de passe incorrect");
     }
-}
+});
