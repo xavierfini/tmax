cdec3ef16f377f87b3fc49ce3820b6752c355967
xavierfini <108425006+xavierfini@users.noreply.github.com>
2024-04-24 17:53:15 +0200

    MAJ

--- scriptconnexion.js
+++ scriptconnexion.js
@@ -1,25 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("loginForm").addEventListener("submit", function (event) {
-        event.preventDefault(); // Empêche l'envoi du formulaire par défaut
+function solve() {
+    var users = [
+        { username: "xavierfini", password: "Xavier14", redirectUrl: "xavierfini.html" },
+        { username: "utilisateur2", password: "motdepasse2", redirectUrl: "page2.html" },
+        // Ajoutez d'autres utilisateurs au besoin
+    ];
 
-        // Votre code JavaScript ici
-        var users = [
-            { username: "xavierfini", password: "Xavier14", redirectUrl: "xavierfini.html" },
-            { username: "utilisateur2", password: "motdepasse2", redirectUrl: "page2.html" },
-            // Ajoutez d'autres utilisateurs au besoin
-        ];
+    var usernameInput = document.getElementById("username");
+    var passwordInput = document.getElementById("password");
 
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
+    var username = usernameInput.value;
+    var password = passwordInput.value;
 
-        var authenticatedUser = users.find(function (user) {
-            return user.username === username && user.password === password;
-        });
-
-        if (authenticatedUser) {
-            window.location.href = authenticatedUser.redirectUrl;
-        } else {
-            alert("Identifiant ou mot de passe incorrect");
-        }
+    var authenticatedUser = users.find(function(user) {
+        return user.username === username && user.password === password;
     });
+
+    if (authenticatedUser) {
+        window.location.href = authenticatedUser.redirectUrl;
+    } else {
+        alert("Identifiant ou mot de passe incorrect");
+    }
+
+    // Empêcher l'envoi du formulaire par défaut
+    return false;
+}
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Empêche l'envoi du formulaire par défaut
+    solve();
 });
