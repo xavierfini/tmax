5c0c09dfb4ebb4ff58aa96372119eeab06b87559
xavierfini <108425006+xavierfini@users.noreply.github.com>
2024-04-24 17:25:48 +0200

    MAJ

--- scriptconnexion.js
+++ scriptconnexion.js
@@ -1,25 +1,25 @@
-// Définition des utilisateurs avec leurs identifiants, mots de passe et pages dédiées
-var users = [
-    { username: "xavierfini", password: "Xavier14", redirectUrl: "xavierfini.html" },
-    { username: "utilisateur2", password: "motdepasse2", redirectUrl: "page2.html" },
-    // Ajoutez d'autres utilisateurs au besoin
-];
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("loginForm").addEventListener("submit", function (event) {
+        event.preventDefault(); // Empêche l'envoi du formulaire par défaut
 
-document.getElementById("loginForm").addEventListener("submit", function(event) {
+        // Votre code JavaScript ici
+        var users = [
+            { username: "xavierfini", password: "Xavier14", redirectUrl: "xavierfini.html" },
+            { username: "utilisateur2", password: "motdepasse2", redirectUrl: "page2.html" },
+            // Ajoutez d'autres utilisateurs au besoin
+        ];
 
-    // Récupérer les valeurs de l'identifiant et du mot de passe
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
 
-    // Vérifier l'authentification pour chaque utilisateur
-    var authenticatedUser = users.find(function(user) {
-        return user.username === username && user.password === password;
+        var authenticatedUser = users.find(function (user) {
+            return user.username === username && user.password === password;
+        });
+
+        if (authenticatedUser) {
+            window.location.href = authenticatedUser.redirectUrl;
+        } else {
+            alert("Identifiant ou mot de passe incorrect");
+        }
     });
-
-    if (authenticatedUser) {
-        // Rediriger vers la page dédiée de l'utilisateur authentifié
-        window.location.href = authenticatedUser.redirectUrl;
-    } else {
-        alert("Identifiant ou mot de passe incorrect");
-    }
 });
